ea9ddee0551dda19141516ee9f93ae0cea0e5475
Grimpascal <armadalautmerah1993@gmail.com>
2026-07-23 19:32:59 +0700

    update dikit

--- src/components/Elements/Footer/medsos.jsx
+++ src/components/Elements/Footer/medsos.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 const Medsos = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-[22px]'>
         <div className='pt-[44px]'>
             <h1 className='text-[#F2F2F2] text-[22px] font-semibold'>Ikuti Kami</h1>
         </div>
-        <div>
-            <a href="" className='bg-[#E0E0E0] p-[8px] rounded-[12px]'></a>
-            <a href="" className='bg-[#E0E0E0] p-[8px] rounded-[12px]'></a>
-            <a href="" className='bg-[#E0E0E0] p-[8px] rounded-[12px]'></a>
+        <div className='gap-[24px] flex'>
+            <a href="" className='bg-[#E0E0E0] p-[8px] rounded-[12px]'><FontAwesomeIcon icon={faYoutube} className='w-[24px] h-[24px]'/></a>
+            <a href="" className='bg-[#E0E0E0] p-[8px] rounded-[12px]'><FontAwesomeIcon icon={faInstagram} className='w-[24px] h-[24px]'/></a>
+            <a href="" className='bg-[#E0E0E0] p-[8px] rounded-[12px]'><FontAwesomeIcon icon={faFacebook} className='w-[24px] h-[24px]'/></a>
         </div>
     </div>
   )
--- src/components/Fragments/cardHeadline.jsx
+++ src/components/Fragments/cardHeadline.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
 
 const CardHeadline = ({berita}) => {
   return (
@@ -7,7 +9,7 @@ const CardHeadline = ({berita}) => {
             <h1 className="font-[600px] font-">Headline</h1>
             <h1 className="font-bold text-[36px] text-left w-[505px] font-[700px] tracking-normal">{berita.title}</h1>
             <p className="text-[16px] w-[505px] h-[99.36px] font-[400px]">{berita.description}</p>
-            <p className="pb-[16px]">{new Date(berita.pubDate).toLocaleString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
+            <p className="pb-[16px]"><FontAwesomeIcon icon={faCalendar} className='pr-[8px]'/>{new Date(berita.pubDate).toLocaleString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
             <a href={berita.link} className="text-[#0090FF] font-[500px] pt-[16px]">Baca Selengkapnya</a>
         </div>
         <div className="w-[637px] w-[174px] h-[48px]">
