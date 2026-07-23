4cdf8314c47751fddc3a6a65d08cd1b652323b99
Grimpascal <armadalautmerah1993@gmail.com>
2026-07-23 19:42:07 +0700

    .

--- src/components/Elements/cardPopuler.jsx
+++ src/components/Elements/cardPopuler.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 
 const CardPopuler = ({berita}) => {
   return (
-    <div className="flex gap-[16px] p-[16px]">
-        <div className="w-[175px] h-[128px]">
-            <img src={berita.thumbnail} alt="thumbnail" className="rounded-[12px]"/>
+    
+    <div className="flex gap-[16px] p-[16px] items-center">
+        <div className="w-[175px] h-[128px] flex-shrink-0">
+            <img src={berita.thumbnail} alt="thumbnail" className="w-full h-full object-cover rounded-[12px]"/>
         </div>
-        <div>
-            <h1 className="w-[205px] text-[13px] h-[75px] font-bold">{berita.title}</h1>
+        <div className="flex flex-col gap-y-[12px] justify-between h-full">
+            <a className="text-[13px] font-bold hover:text-[#0090FF] transition-colors line-clamp-2" href={berita.link}>
+                {berita.title}
+            </a>
             <div className="flex gap-[12px] items-center">
                 <p className="text-[14px] text-[#0090FF] font-semibold">Bola</p>
-                <p className="text-[#526071]">●</p>
-                <p className="text-[14px] text-[#526071]">{new Date(berita.pubDate).toLocaleString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
+                <p className="text-[#526071] text-[10px]">●</p>
+                <p className="text-[14px] text-[#526071]">
+                    {new Date(berita.pubDate).toLocaleString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}
+                </p>
             </div>
-        </div>
-        <div>
         </div>
     </div>
   )
