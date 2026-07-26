ea5a1f112613db4feebdc04b027f5d21f0357c62
Grimpascal <armadalautmerah1993@gmail.com>
2026-07-26 20:52:39 +0700

    update dikit

--- src/components/Fragments/cardHeadline.jsx
+++ src/components/Fragments/cardHeadline.jsx
@@ -7,7 +7,7 @@ const CardHeadline = ({berita}) => {
     <div className="flex gap-[154px] pt-[72px]">
         <div>
             <h1 className="font-[600px] font-">Headline</h1>
-            <h1 className="font-bold text-[36px] text-left w-[505px] font-[700px] tracking-normal">{berita.title}</h1>
+            <h1 className="font-bold text-[36px] text-left w-[505px] font-[700px] tracking-normal"><a href={berita.link}>{berita.title}</a></h1>
             <p className="text-[16px] w-[505px] h-[99.36px] font-[400px]">{berita.description}</p>
             <p className="pb-[16px]"><FontAwesomeIcon icon={faCalendar} className='pr-[8px]'/>{new Date(berita.pubDate).toLocaleString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' })}</p>
             <a href={berita.link} className="text-[#0090FF] font-[500px] pt-[16px]">Baca Selengkapnya</a>
